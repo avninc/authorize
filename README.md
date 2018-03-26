6b7fc17c4271a62deb0092ae988a2da5ad76818b
Vincent Gabriel <vadimg88@gmail.com>
2018-03-26 12:11:30 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# authorize
+
+[![Build Status](https://travis-ci.org/avninc/authorize.svg?branch=master)](https://travis-ci.org/avninc/authorize)
+[![Coverage Status](https://coveralls.io/repos/github/avninc/authorize/badge.svg?branch=master)](https://coveralls.io/github/avninc/authorize?branch=master)
+
+
---
+++ test/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Phpunit bootstrap file.
+ * 
+ * @license http://opensource.org/licenses/mit-license.php The MIT License (MIT)
+ * @link https://github.com/avninc/authorize
+ * @author Vincent Gabriel <vincent.gabriel@avn.com>
+ */
+
+$loader = require __DIR__ . '/../vendor/autoload.php';
+$loader->addPsr4('Fixture\\', 'test/Fixture');
